e456d75d43e4fc75922e20a3b4d5264a61feb3b0
shioko <ichisadashioko@gmail.com>
2025-03-08 16:28:20 +0700

    flip sprite in moving direction

---
+++ Assets/HE200901/FlipSpriteInMovingDirection.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class FlipSpriteInMovingDirection : MonoBehaviour
+{
+    private float previous_x = 0;
+    public bool DefaultFacingDirectionIsRight = true;
+
+    void Update()
+    {
+        float current_x = gameObject.transform.position.x;
+        if(current_x > previous_x)
+        {
+            // moving right
+            FaceRight();
+        }else if(current_x < previous_x)        {
+            // moving left
+            FaceLeft();
+        }
+        previous_x = current_x;
+    }
+
+    void FaceRight()
+    {
+        float _tmp_indicator = 1;
+        if (!DefaultFacingDirectionIsRight)
+        {
+            _tmp_indicator = -1;
+        }
+
+        Vector3 _scale = gameObject.transform.localScale;
+        var _scale_x = _scale.x;
+        _scale_x = _tmp_indicator * Mathf.Abs(_scale_x);
+        _scale.x = _scale_x;
+        gameObject.transform.localScale = _scale;
+    }
+    void FaceLeft()
+    {
+        float _tmp_indicator = -1;
+        if (!DefaultFacingDirectionIsRight)
+        {
+            _tmp_indicator = 1;
+        }
+
+        Vector3 _scale = gameObject.transform.localScale;
+        var _scale_x = _scale.x;
+        _scale_x = _tmp_indicator * Mathf.Abs(_scale_x);
+        _scale.x = _scale_x;
+        gameObject.transform.localScale = _scale;
+    }
+}
