1ba9a05bd1e73f5a477ddcf260f42591abd49abc
shioko <ichisadashioko@gmail.com>
2025-03-15 17:46:54 +0700

    stash changes

--- Assets/Scripts/RemovedOnTouchPlayer.cs
+++ Assets/Scripts/RemovedOnTouchPlayer.cs
@@ -14,7 +14,48 @@ public class RemovedOnTouchPlayer : MonoBehaviour
     }
     public float update_interval = 0.1f;
     private float last_update_delta_time = 0;
+    public float melee_damage_deal_time = 0;
+    public float melee_damage_deal_interval = 1f;
 
+    public bool collide_with_player(){
+        if(player == null){
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        if (player != null)
+        {
+            Vector3 player_position = player.transform.position;
+            Vector3 player_size = player.GetComponent<SpriteRenderer>().bounds.size;
+
+            float player_left_x = player_position.x - player_size.x / 2;
+            float player_right_x = player_position.x + player_size.x / 2;
+            float player_top_y = player_position.y - player_size.y / 2;
+            float player_bottom_y = player_position.y + player_size.y / 2;
+
+
+            Vector3 gameObject_position = gameObject.transform.position;
+            Vector3 gameObject_size = gameObject.GetComponent<SpriteRenderer>().bounds.size;
+
+            float gameObject_left_x = gameObject_position.x - gameObject_size.x / 2;
+            float gameObject_right_x = gameObject_position.x + gameObject_size.x / 2;
+            float gameObject_top_y = gameObject_position.y - gameObject_size.y / 2;
+            float gameObject_bottom_y = gameObject_position.y + gameObject_size.y / 2;
+
+            Rect player_rect = new Rect(player_left_x, player_top_y, player_size.x, player_size.y);
+            //player_rect.
+            //Rect.Inter
+            int int_scale = 1000;
+            Rectangle player_rectangle = new Rectangle((int)(player_left_x * int_scale), (int)(player_top_y * int_scale), (int)(player_size.x * int_scale), (int)(player_size.y * int_scale));
+            //Rectangle gameObject_rectangle = new Rectangle((int)(player_left_x * int_scale), (int)(player_top_y * int_scale), (int)(player_size.x * int_scale), (int)(player_size.y * int_scale));
+            Rectangle gameObject_rectangle = new Rectangle((int)(gameObject_left_x * int_scale), (int)(gameObject_top_y * int_scale), (int)(gameObject_size.x * int_scale), (int)(gameObject_size.y * int_scale));
+            if (!Rectangle.Intersect(player_rectangle, gameObject_rectangle).IsEmpty)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -22,53 +63,18 @@ public class RemovedOnTouchPlayer : MonoBehaviour
         if (last_update_delta_time > update_interval)
         {
             last_update_delta_time = 0;
-
-            if (player != null)
-            {
-
-                Vector3 player_position = player.transform.position;
-                Vector3 player_size = player.GetComponent<SpriteRenderer>().bounds.size;
-
-                float player_left_x = player_position.x - player_size.x / 2;
-                float player_right_x = player_position.x + player_size.x / 2;
-                float player_top_y = player_position.y - player_size.y / 2;
-                float player_bottom_y = player_position.y + player_size.y / 2;
-
-
-                Vector3 gameObject_position = gameObject.transform.position;
-                Vector3 gameObject_size = gameObject.GetComponent<SpriteRenderer>().bounds.size;
-
-                float gameObject_left_x = gameObject_position.x - gameObject_size.x / 2;
-                float gameObject_right_x = gameObject_position.x + gameObject_size.x / 2;
-                float gameObject_top_y = gameObject_position.y - gameObject_size.y / 2;
-                float gameObject_bottom_y = gameObject_position.y + gameObject_size.y / 2;
-
-                Rect player_rect = new Rect(player_left_x, player_top_y, player_size.x, player_size.y);
-                //player_rect.
-                //Rect.Inter
-                int int_scale = 1000;
-                Rectangle player_rectangle = new Rectangle((int)(player_left_x * int_scale), (int)(player_top_y * int_scale), (int)(player_size.x * int_scale), (int)(player_size.y * int_scale));
-                //Rectangle gameObject_rectangle = new Rectangle((int)(player_left_x * int_scale), (int)(player_top_y * int_scale), (int)(player_size.x * int_scale), (int)(player_size.y * int_scale));
-                Rectangle gameObject_rectangle = new Rectangle((int)(gameObject_left_x * int_scale), (int)(gameObject_top_y * int_scale), (int)(gameObject_size.x * int_scale), (int)(gameObject_size.y * int_scale));
-                if (!Rectangle.Intersect(player_rectangle, gameObject_rectangle).IsEmpty)
+            if(collide_with_player()){
+                // ObjectPoolManager.ReturnGameObjectToPool(gameObject);
+                // SpawnEnemies.CURRENT_ACTIVE_ENEMIES_COUNT -= 1;
+                float current_time = Time.time;
+                float melee_damage_deal_delta_time = current_time - melee_damage_deal_time;
+                if(melee_damage_deal_delta_time > melee_damage_deal_interval)
                 {
-                    ObjectPoolManager.ReturnGameObjectToPool(gameObject);
-                    SpawnEnemies.CURRENT_ACTIVE_ENEMIES_COUNT -= 1;
-                    var player_hp_state = player.GetComponent<PlayerHP>();
-                    if (player_hp_state != null)
-                    {
-                        player_hp_state.on_hit(1);
-                    }
-                    Die();
+                    melee_damage_deal_time = current_time;
+                    GameState.CURRENT_HP -= 1;
                 }
             }
         }
-    }
-    private void Die()
-    {
-        DropExp();
-        ObjectPoolManager.ReturnGameObjectToPool(gameObject);
-
     }
 
     private GameObject expPrefab;
