f8c01de4a5bdf91634c23313756d31beacf87562
shioko <ichisadashioko@gmail.com>
2025-03-15 17:50:36 +0700

    run formatting script

--- Assets/Scripts/HPForEnermy.cs
+++ Assets/Scripts/HPForEnermy.cs
@@ -5,13 +5,13 @@ public class HPForEnermy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public float HP { get; set; }
--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -11,7 +11,7 @@ public class PlayerHP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public int max_hp = 20;
