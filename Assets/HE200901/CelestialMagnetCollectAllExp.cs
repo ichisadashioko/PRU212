a6244a28b7fcff692046e754aad79c8674624c98
shioko <ichisadashioko@gmail.com>
2025-03-16 19:17:29 +0700

    show collected exp and hp using magnet

--- Assets/HE200901/CelestialMagnetCollectAllExp.cs
+++ Assets/HE200901/CelestialMagnetCollectAllExp.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class CelestialMagnetCollectAllExp : MonoBehaviour
@@ -22,15 +23,43 @@ public class CelestialMagnetCollectAllExp : MonoBehaviour
         if (UnityGameUtility.check_collision(player, gameObject))
         {
             ObjectPoolManager.ReturnGameObjectToPool(gameObject);
+            int total_exp = 0;
             var exp_obj_list = GameObject.FindGameObjectsWithTag("ExpOrb");
-            foreach(GameObject exp_obj in exp_obj_list)
+            foreach (GameObject exp_obj in exp_obj_list)
             {
                 if (exp_obj.activeInHierarchy)
                 {
                     ObjectPoolManager.ReturnGameObjectToPool(exp_obj);
-                    ExperienceManager.Instance.AddExperience(20);
+                    int exp_value = 20;
+                    ExperienceManager.Instance.AddExperience(exp_value);
+                    total_exp += exp_value;
                 }
             }
+
+            if (total_exp > 0)
+            {
+
+                GameObject damage_popup_prefab = Resources.Load<GameObject>("damage_popup");
+                if (damage_popup_prefab != null)
+                {
+                    var _damage_popup = ObjectPoolManager.SpawnNewGameObject(damage_popup_prefab, player.transform.position, Quaternion.identity, ObjectPoolManager.PoolType.Text);
+                    var _tmp = _damage_popup.GetComponent<TextMeshPro>();
+                    if (_tmp != null)
+                    {
+                        _tmp.text = $"{total_exp} EXP";
+                        //_tmp.color = new UnityEngine.Color(1f, 1f, 0f);
+                        _tmp.color = new Color32(0, 255, 128, 255);
+                        _tmp.faceColor = new Color32(0, 255, 128, 255);
+                    }
+                    var _fade = _damage_popup.GetComponent<DamagePopupFade>();
+                    if (_fade != null)
+                    {
+                        _fade.created_time = Time.time;
+                    }
+                }
+            }
+
+            int total_restored_hp = 0;
             var hp_obj_list = GameObject.FindGameObjectsWithTag("restore_hp_item");
             foreach (GameObject hp_obj in hp_obj_list)
             {
@@ -38,6 +67,37 @@ public class CelestialMagnetCollectAllExp : MonoBehaviour
                 {
                     ObjectPoolManager.ReturnGameObjectToPool(hp_obj);
                     GameState.CURRENT_HP += 5;
+                    total_restored_hp += 5;
+                }
+            }
+            if(total_restored_hp > 0)
+            {
+                GameObject damage_popup_prefab = Resources.Load<GameObject>("damage_popup");
+                if (damage_popup_prefab != null)
+                {
+                    var _damage_popup = ObjectPoolManager.SpawnNewGameObject(damage_popup_prefab, player.transform.position, Quaternion.identity, ObjectPoolManager.PoolType.Text);
+                    var _tmp = _damage_popup.GetComponent<TextMeshPro>();
+                    if (_tmp != null)
+                    {
+                        _tmp.text = $"+{total_restored_hp} HP";
+                        //_tmp.color = new UnityEngine.Color(1f, 1f, 0f);
+                        _tmp.color = new Color32(0, 255, 0, 255);
+                        _tmp.faceColor = new Color32(0, 255, 0, 255);
+                    }
+                    var _fade = _damage_popup.GetComponent<DamagePopupFade>();
+                    if (_fade != null)
+                    {
+                        _fade.created_time = Time.time;
+                    }
+                }
+            }
+
+            var obj_list = GameObject.FindGameObjectsWithTag("magnet_item");
+            foreach (GameObject go_obj in obj_list)
+            {
+                if (go_obj.activeInHierarchy)
+                {
+                    ObjectPoolManager.ReturnGameObjectToPool(go_obj);
                 }
             }
         }
