018bcc41a0f5d1b012da64912e887cd4fa33cf8b
ToanLV <toanlvhe173348@fpt.edu.vn>
2025-03-17 17:05:31 +0700

    Add ui game over

---
+++ Assets/toan/GameOver.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+public class GameOver : MonoBehaviour
+{
+    private UIDocument _document;
+    private Button restart_btn;
+    private Button menu_btn;
+    void Start()
+    {
+        _document = GetComponent<UIDocument>();
+        restart_btn = _document.rootVisualElement.Q("btn_restart") as Button;
+        menu_btn = _document.rootVisualElement.Q("btn_menu") as Button;
+        restart_btn.RegisterCallback<ClickEvent>(restart_button_clicked);
+        menu_btn.RegisterCallback<ClickEvent>(menu_button_clicked);
+    }
+
+    private void restart_button_clicked(ClickEvent evt)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    private void menu_button_clicked(ClickEvent evt)
+    {
+        SceneManager.LoadScene("Start_Game");
+    }
+
+    void Update()
+    {
+
+    }
+}
