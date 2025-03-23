f78043ec62a2f215da2c24e72bd88dbff59120ae
HE171702_DUC <ducapple1232@gmail.com>
2025-03-23 17:12:14 +0700

    duc: add animation to player

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    public Animator ani = null;
+    float move;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +16,10 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ani == null)
+        {
+            ani = GetComponent<Animator>();
+        }
         float left_x = 0;
         float right_x = 0;
         float top_y = 0;
@@ -34,30 +40,35 @@ public class MovePlayer : MonoBehaviour
                 has_world_border = true;
             }
         }
+        move = 0;
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             Vector3 position = this.transform.position;
             position.x -= move_speed * Time.deltaTime;
             this.transform.position = position;
+            move = 1;
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
             Vector3 position = this.transform.position;
             position.x += move_speed * Time.deltaTime;
             this.transform.position = position;
+            move = 1;
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             Vector3 position = this.transform.position;
             position.y += move_speed * Time.deltaTime;
             this.transform.position = position;
+            move = 1;
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             Vector3 position = this.transform.position;
             position.y -= move_speed * Time.deltaTime;
             this.transform.position = position;
+            move = 1;
         }
 
         if (has_world_border)
@@ -66,6 +77,20 @@ public class MovePlayer : MonoBehaviour
             current_position.x = Mathf.Clamp(current_position.x, left_x, right_x);
             current_position.y = Mathf.Clamp(current_position.y, top_y, bottom_y);
             transform.position = current_position;
+        }
+        else
+        {
+            move = 0;
+        }
+
+        if (ani != null)
+        {
+            ani.SetFloat("Dichuyen", move);
+
+        }
+        else
+        {
+            Debug.Log("no animator");
         }
     }
 }
